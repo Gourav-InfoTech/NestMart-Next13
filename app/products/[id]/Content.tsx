abdda095c9d37f1d15f9c089472a50b04c34f07f
Gourav <gourav.delta4infotech@gmail.com>
2023-03-13 17:37:41 +0530

    error fixed

--- app/products/[id]/Content.tsx
+++ app/products/[id]/Content.tsx
@@ -10,21 +10,21 @@ const Content = ({ data }: { data: any }) => {
       <div className="item">
         <div>
           <div className="item_image ">
-            <img src={data.image} alt="" />
+            <img src={data?.image} alt="" />
           </div>
-          <p className="text-xs py-2 text-[#adadad]">{data.category}</p>
+          <p className="text-xs py-2 text-[#adadad]">{data?.category}</p>
           <h2 className="pb-2 text-[15px] font-[Quicksand] font-bold text-[#253d4e] leading-5">
-            {data.title}
+            {data?.title}
           </h2>
           <p className="pb-4 text-[#adadad] text-sm">
-            ⭐⭐⭐⭐⭐({data.rating.rate})
+            ⭐⭐⭐⭐⭐({data?.rating.rate})
           </p>
           {/* <p className="text-[#adadad] text-sm">By <span></span> </p> */}
         </div>
 
         <div className="price_&_button flex justify-between gap-3">
           <p className="text-[#3bb77e] font-bold font-[Quicksand]">
-            ${data.price}
+            ${data?.price}
           </p>
           <button className="flex gap-2 items-center py-[6px] px-[15px] rounded-sm bg-[#def9ec] text-[#3bb77e] hover:bg-[#3bb77e] hover:text-[#def9ec] hover:-translate-y-1 transition-all duration-300">
             <HiOutlineShoppingCart />
