afc7504520a053275ea7a2c853eadbfff0915eb8
Gourav <gourav.delta4infotech@gmail.com>
2023-03-27 10:13:06 +0530

    key added in blog

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -2,29 +2,32 @@ import Link from "next/link";
 import React from "react";
 
 const page = () => {
-
   const arr = [
     {
       id: 1,
     },
     {
-      id: 2
+      id: 2,
     },
     {
-      id: 3
+      id: 3,
     },
-  ]
+  ];
 
-  return <div className="common_width section_padding"> 
-    <h4>
-      blog ( different layout )
-      </h4>
-      {
-        arr.map((e,i)=>{
-          return <Link href={`/blog/${e.id}`}><li><button>Blog no.{e.id}</button></li></Link>
-        })
-      }
+  return (
+    <div className="common_width section_padding">
+      <h4>blog ( different layout )</h4>
+      {arr.map((e, i) => {
+        return (
+          <Link key={i} href={`/blog/${e.id}`}>
+            <li>
+              <button>Blog no.{e.id}</button>
+            </li>
+          </Link>
+        );
+      })}
     </div>
+  );
 };
 
 export default page;
